b345cad009031864bd72dd1045349f9e46d7d22d
Adem Ben Amor <dissojak@gmail.com>
2023-08-23 22:19:21 +0100

    Update the Custom Hook of hhtp Request v3

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,27 +1,28 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-  const activeHttpRequests = []; // Using an array to store controllers
+  const activeHttpRequests = useRef([]);
 
   const sendRequest = useCallback(
     async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
-      const httpAbortController = new AbortController(); // Create a new controller for each request
-      activeHttpRequests.push(httpAbortController); // Store the controller
+      const httpAbortController = new AbortController();
+      activeHttpRequests.current.push(httpAbortController);
 
       try {
         const response = await fetch(url, {
           method,
           body,
           headers,
-          signal: httpAbortController.signal, // Use the signal from the controller
+          signal: httpAbortController.signal,
         });
         const responseData = await response.json();
 
-        // Remove the used controller from the array
-        activeHttpRequests.splice(activeHttpRequests.indexOf(httpAbortController), 1);
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (ctrl) => ctrl !== httpAbortController
+        );
 
         if (!response.ok) {
           throw new Error(responseData.message);
@@ -41,11 +42,9 @@ export const useHttp = () => {
     setError(null);
   };
 
-  // Abort all pending requests when the component unmounts
-  // This ensures that no pending requests will update the state after the component has unmounted
   useEffect(() => {
     return () => {
-      activeHttpRequests.forEach((abortCtrl) => {
+      activeHttpRequests.current.forEach((abortCtrl) => {
         abortCtrl.abort();
       });
     };

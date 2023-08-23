6b8de4beff01a7651a963e3c3e197bd5553d84c7
Adem Ben Amor <dissojak@gmail.com>
2023-08-23 22:19:54 +0100

    update http-request of user !

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,39 +3,29 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttp } from "../../shared/hooks/http-hook";
 
 const Users = () => {
   // 'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttp();
   const [users, setUsers] = useState();
 
+
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const req = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/user/");
-        const responseData = await response.json();
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest("http://localhost:5000/api/user/");
         setUsers(responseData.users);
       } catch (e) {
-        setError(e.message);
       }
-      setIsLoading(false);
     };
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    req();
+  }, [sendRequest]);
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />

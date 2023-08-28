7ff54dc7762580d7288b47a4dcb82e25da5d0d10
Adem Ben Amor <dissojak@gmail.com>
2023-08-28 22:28:26 +0100

    make it clear & comments

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,48 +1,57 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./ImageUpload.css";
-import Button from "./Button";
+import Button from "./Button"; 
 
 const ImageUpload = (props) => {
+  // Creating a reference to the file input element
   const filePickerRef = useRef();
+
+  // State variables to manage the selected file, image URL, and validity
   const [file, setFile] = useState();
   const [url, setUrl] = useState();
   const [isValid, setIsValid] = useState(false);
 
+  // useEffect to update the image URL when a new file is selected
   useEffect(() => {
     if (!file) {
       return;
     }
 
+    // Creating a new FileReader to read the selected file
     const fileReader = new FileReader();
     fileReader.onload = () => {
-      setUrl(fileReader.result);
+      setUrl(fileReader.result); // Setting the URL to the read data
     };
-    fileReader.readAsDataURL(file);
+    fileReader.readAsDataURL(file); // Initiating the read operation
   }, [file]);
 
+  // Function to handle the selection of a file
   const pickedHandler = (event) => {
     let pickedFile;
     let valid;
 
     if (event.target.files && event.target.files.length === 1) {
       pickedFile = event.target.files[0];
-      setFile(pickedFile);
-      setIsValid(true);
+      setFile(pickedFile); // Setting the selected file
+      setIsValid(true); // Marking the input as valid
       valid = true;
     } else {
-      setIsValid(false);
+      setIsValid(false); // Marking the input as invalid
       valid = false;
     }
 
+    // Calling the onInput function from props to communicate with parent component
     props.onInput(props.id, pickedFile, valid);
   };
 
+  // Function to trigger the file input when the "Pick Image" button is clicked
   const pickImageHandler = () => {
-    filePickerRef.current.click();
+    filePickerRef.current.click(); // Programmatically triggering the file input click
   };
 
   return (
     <div className="form-control">
+      {/* Hidden file input element */}
       <input
         id={props.id}
         ref={filePickerRef}
@@ -51,15 +60,22 @@ const ImageUpload = (props) => {
         type="file"
         onChange={pickedHandler}
       />
+
+      {/* Container for the image upload UI */}
       <div className={`image-upload ${props.center && "center"}`}>
+        {/* Preview of the selected image */}
         <div className="image-upload__preview">
-          {url && <img src={url} alt="Preview" />}
-          {!url && <p>Please pick an image.</p>}
+          {url && <img src={url} alt="Preview" />} {/* Displaying the image if URL exists */}
+          {!url && <p>Please pick an image.</p>} {/* Message if no image is selected */}
         </div>
+
+        {/* Button to initiate image selection */}
         <Button type="button" onClick={pickImageHandler}>
           Pick Image
         </Button>
       </div>
+
+      {/* Displaying an error message if input is invalid */}
       {!isValid && <p>{props.errorText}</p>}
     </div>
   );
